0ba607d74b6a3f9a3e564640bcebc90e5295687a
jreleaserbot <jreleaser@kordamp.org>
2022-04-07 13:21:37 +0000

    sdkman-cli 5.15.0

--- Formula/sdkman-cli.rb
+++ Formula/sdkman-cli.rb
@@ -1,15 +1,15 @@
 class SdkmanCli < Formula
   desc "Sdkman - The Software Development Kit Manager"
   homepage "https://sdkman.io"
-  url "https://github.com/sdkman/sdkman-cli/releases/download/5.14.3/sdkman-cli-5.14.3.zip"
-  version "5.14.3"
-  sha256 "c5130c787b90ffa78470b148f90792ba376a07c93958eaba1a97da9e20239cae"
+  url "https://github.com/sdkman/sdkman-cli/releases/download/5.15.0/sdkman-cli-5.15.0.zip"
+  version "5.15.0"
+  sha256 "fb5ef39fed3d5eaea70b6c06fb4e5807f13e5c625b592ff6d1b0ab373e5a4408"
   license "Apache-2.0"
 
   def install
     libexec.install Dir["*"]
 
-    %w[tmp ext etc var archives candidates].each { |dir| mkdir libexec/dir }
+    %w[tmp ext etc var candidates].each { |dir| mkdir libexec/dir }
 
     system "curl", "-s", "https://api.sdkman.io/2/candidates/all", "-o", libexec/"var/candidates"
 
